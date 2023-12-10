9d00d88598df0827345015fe30c4f90d555f5224
jacobhcs <jaharr1999@gmail.com>
2023-12-10 02:16:29 -0500

    my solution using a for loop to iterate through the array.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var block = document.querySelector('#removethis');
 var image = document.querySelector('#photo');
 var select = document.querySelector('#menu');
-var degrees = document.querySelectorAll('.degree');
-var tempSelect = document.querySelector('#measurements');
-var fahrenheitTemps = Array.from(degrees).map(deg => deg.innerText);
+var degree = document.querySelectorAll('.degree')
+var tempSelect = document.querySelector('#measurements')
+
+
 
 function removeBlock() {
   block.remove();
@@ -26,20 +27,24 @@ function releaseMe(element) {
 }
 
 var image = document.querySelector('#photo');
-var select = document.querySelector('#menu');
+var select = document.querySelector('#menu')
 
 function imageChange() {
   image.src = select.value;
 }
 
 function changeMeasure() {
+  var fahrenheitTemp = ['70','90'];
   if (tempSelect.value == 'C') {
-    degrees.forEach((degree, index) => {
-      degree.innerText = Math.floor((parseFloat(fahrenheitTemps[index]) - 32) * 5 / 9);
-    });
+    for (var i = 0; i < degree.length; i++) {
+      fahrenheitTemp.push(degree[i].innerText);
+      degree[i].innerText = Math.floor((degree[i].innerText - 32) * 5 / 9);
+      console.log(degree[i].innerText);
+    }
   } else if (tempSelect.value == 'F') {
-    degrees.forEach((degree, index) => {
-      degree.innerText = fahrenheitTemps[index];
-    });
+    for (var x = 0; x < degree.length; x++) {
+      degree[x].innerText = fahrenheitTemp[x];
+      console.log(degree[x].innerText);
+    }
   }
 }
