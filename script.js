66725e47acd84b8970c005dbac6478713806d297
jacobhcs <jaharr1999@gmail.com>
2023-12-12 14:13:52 -0500

    made some notes to study javascript with.

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
 var block = document.querySelector('#removethis');
-var image = document.querySelector('#photo');
-var select = document.querySelector('#menu');
-var degree = document.querySelectorAll('.degree');
-var tempSelect = document.querySelector('#measurements');
-var searchbutton = document.querySelector('.searchbutton');
-var searchbar = document.querySelector('#searchbar');
 
-
+// when button is clicked the box will be romved.
 function removeBlock() {
   block.remove();
 }
 
+// will change box to blue when hovering over.
 function over(element) {
   element.style.backgroundColor = 'blue';
 }
 
+// will change style of box backgroundColor to red when cursor is outside of box.
 function out(element) {
   element.style.backgroundColor = 'red';
 }
 
+// will change the box backgroundColor to green when box is clicked
 function clickMe(element) {
   element.style.backgroundColor = 'green';
 }
 
+// will change box backgroundColor to black when click is released
 function releaseMe(element) {
   element.style.backgroundColor = 'black';
 }
 
+// grabbing the select and img element with the id name #menu and #photo from html to reference in the function.
 var image = document.querySelector('#photo');
 var select = document.querySelector('#menu')
 
+// this function changes the photo (img src) being displayed on the screen when you <select> a new value.
 function imageChange() {
   image.src = select.value;
 }
 
+// grabbing all paragraph element with the class of .degree to use in function changeMeasure.
+var degree = document.querySelectorAll('.degree');
+// grabbing the select element that changes the temperature measurements.
+var tempSelect = document.querySelector('#measurements');
+
+// this function changes the values of all the temperature measurements on the weather cards.
 function changeMeasure() {
   var fahrenheitTemp = ['70', '90'];
   if (tempSelect.value == 'C') {
     for (var i = 0; i < degree.length; i++) {
-      // fahrenheitTemp.push(degree[i].innerText);
       degree[i].innerText = Math.floor((degree[i].innerText - 32) * 5 / 9);
       console.log(degree[i].innerText);
     }
@@ -50,19 +55,34 @@ function changeMeasure() {
   }
 }
 
+// referencing the input and button elements to include in the function alertValue().
+var searchbutton = document.querySelector('.searchbutton');
+var searchbar = document.querySelector('#searchbar');
+
+// this function will prompt an alert message the will be the value of the text written inside the searchbar.
 function alertValue() {
   alert(`Searching for "${searchbar.value}"`)  
 }
 
-var removebtn = document.querySelector('.removebtn');
-
+// this will remove the button that is being clicked using the 'this' keyword in html argument.
 function removeButton(element) {
   element.remove()
 }
 
-var counterstart = document.querySelector('counter')
+// this is grabbing the p.counter element from the element to modify its inner text value.
+var counterstart = document.querySelector('.counter')
+
+// this will be our counter that increases or decreases by 1 and will be used to change the inner text of the counterstart variable.
 var count = 0;
 
+// this function will INCREASE the count of the counter by 1.
 function countUp(){
-  counterstart.innerText = count++
+  count++;
+  counterstart.innerText = count;
+}
+
+// this function will LOWER the count of the counter by 1.
+function countDown(){
+  count--;
+  counterstart.innerText = count;
 }
